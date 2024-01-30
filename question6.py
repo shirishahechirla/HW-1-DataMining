84d9a37321d1627669f0510a10617f21b75867e9
shirishahechirla <156143385+shirishahechirla@users.noreply.github.com>
2024-01-29 23:45:02 -0500

    Update question6.py

--- question6.py
+++ question6.py
@@ -1,15 +1,21 @@
 def question6_1():
-    """
     answer = {
-        'equal_width': ['string', integer],
-        'equal_frequency': ['string', integer]
+        'equal_width': ['no change', 3],
+        'equal_frequency': ['no change', 3]
     }
-    """
     return answer
 
 def question6_2():
+    answer = {
+        'equal_width': ['no change', 7],
+        'equal_frequency': ['no change', 9]
+    }
     return answer
 
 def question6_3():
+    answer = {
+        'equal_width': ['no change', 2],
+        'equal_frequency': ['no change', 9]
+    }
     return answer 
 
