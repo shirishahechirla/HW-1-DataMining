85807ab7651e14eda098c759126458cfc25e9ca7
shirishahechirla <156143385+shirishahechirla@users.noreply.github.com>
2024-01-29 23:51:08 -0500

    Update question10.py

--- question10.py
+++ question10.py
@@ -1,31 +1,37 @@
 def question10_1():
-    """
-    answer: python bool (True or False)
-    """
+    answer: bool = False
     return answer
 
 def question10_2():
+    answer: bool= True
     return answer
 
 def question10_3():
+    answer: bool = False
     return answer
 
 def question10_4():
+    answer: bool = False
     return answer
 
 def question10_5():
+    answer: bool = False
     return answer
 
 def question10_6():
+    answer: bool = False
     return answer
 
 def question10_7():
+    answer: bool = False
     return answer
 
 def question10_8():
+    answer: bool = False
     return answer
 
 def question10_9():
+    answer: bool = False
     return answer
 
 def question10_10():
